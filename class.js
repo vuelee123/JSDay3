3d0cd2408be6bbe63b771bdb31ca7f9b742f887d
Lee Vue <vuelee123@gmail.com>
2023-04-05 15:18:20 -0400

    JS Day 3

---
+++ class.js
@@ -0,0 +1,37 @@
+// // for in loop
+
+// let fruits = ["apple", 'banana', 'orange', 'mango'];
+
+// for (let i in fruits) {
+//     console.log(fruits[i]);
+// }
+
+// for of loop
+
+// let fruits = ["apple", 'banana', 'orange', 'mango'];
+// for (let fruit of fruits) {
+//     console.log(fruits);
+// }
+
+// Maps
+
+// let myMap = new Map();
+// myMap.set("Dog", "Charles");
+// myMap.set('Cat', 'Meowington III');
+// myMap.set('fish', 'Jose');
+// myMap.set('Lizard', 'Martin');
+// console.log(myMap);
+
+// myMap.set(23432132, 'Jerry Billings');
+// console.log(myMap.size);
+
+// myMap.has('pig');
+
+// functions
+
+//  function multiply (x,y) {
+//     let product = x * y;
+//      return product;
+//  }
+//  console.log(multiply(40,4));
+
