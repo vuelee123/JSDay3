db614dbf9b02d01477e8e95da7f1d92dda93eb4a
vuelee123 <127789133+vuelee123@users.noreply.github.com>
2023-04-06 15:18:37 -0400

    Add files via upload

---
+++ mapsAndSets.js
@@ -0,0 +1,28 @@
+// Practicing Maps and Sets JS day 3
+
+let fruitInventory = new Map ();
+
+    fruitInventory.set('apple', 10);
+    fruitInventory.set('banana', 20);
+    fruitInventory.set ('orange', 30);
+
+fruitInventory.set('apple', 15);
+console.log(fruitInventory);
+
+var bananaInventory = fruitInventory.get ('banana');
+console.log('banana supply' + ' ' + bananaInventory);
+
+let uniqueColors = new Set ();
+uniqueColors.add('red');
+uniqueColors.add('blue');
+uniqueColors.add('green');
+
+console.log(uniqueColors);
+
+uniqueColors.add('yellow');
+
+console.log(uniqueColors);
+
+console.log(uniqueColors.has(1));
+
+let hasBlue = uniqueColors.has('blue');
